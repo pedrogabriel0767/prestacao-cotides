5820bb417cdc7b20853ecf930da0aa1c185bf7ac
unknown <gracigraciele65@gmail.com>
2026-06-21 11:05:56 -0300

    Sistema com banco dinâmico

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-const compras = [
-    {
-        estabelecimento: "Mercado",
-        valor: 100,
-        data: "21/06/2026"
-    },
-    {
-        estabelecimento: "Farmácia",
-        valor: 200,
-        data: "20/06/2026"
-    }
-];
+const URL = "https://raw.githubusercontent.com/pedrogabriel0767/prestacao-cotides/main/data.json";
 
-const tabela = document.getElementById("tabelaCompras");
+async function carregarDados() {
+    const res = await fetch(URL);
+    const data = await res.json();
 
-compras.forEach(compra => {
-    tabela.innerHTML += `
-    <tr>
-        <td>${compra.estabelecimento}</td>
-        <td>R$ ${compra.valor}</td>
-        <td>${compra.data}</td>
-    </tr>
-    `;
-});
+    document.getElementById("saldo").innerText = "R$ " + data.saldo;
+
+    const tabela = document.getElementById("tabelaCompras");
+
+    let total = 0;
+
+    data.compras.forEach(c => {
+        total += c.valor;
+
+        tabela.innerHTML += `
+        <tr>
+            <td>${c.estabelecimento}</td>
+            <td>R$ ${c.valor}</td>
+            <td>${c.data}</td>
+        </tr>
+        `;
+    });
+
+    document.getElementById("total").innerText = "R$ " + total;
+    document.getElementById("quantidade").innerText = data.compras.length;
+}
+
+carregarDados();
